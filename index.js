8850f9735b92f3851271204066bc20c0680b4c6d
Damika <damikaanupama@gmail.com>
2022-12-11 01:00:24 +0530

    Test setup to get users

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const routes = require("./routes/routes");
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -9,6 +10,14 @@ app.use(
 );
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
+});
+app.use("/user", routes);
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({ message: err.message });
+  return;
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
